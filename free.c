4d3d4724ccb939def2a587f24ff05db53daef211
eugene kimani <ekiukkim90@gmail.com>
2023-08-24 08:27:19 -0700

    monty

--- free.c
+++ free.c
@@ -5,3 +5,15 @@
  * @head: head stack of linked list
  */
 
+void stack_free(stack_t *head)
+{
+	stack_t *tmp;
+	tmp = head;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
