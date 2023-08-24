8db8dfb218c82a97b4e12ab4c22dafe956e54aa5
eugene kimani <ekiukkim90@gmail.com>
2023-08-24 08:12:44 -0700

    pythonmonty

--- exec.c
+++ exec.c
@@ -19,3 +19,26 @@ int executor(char *content, stack_t **stack, unsigned int line_no, FILE *file)
 	unsigned int a = 0;
 
 	op_code = strtok(content, "\n\t");
+	if (op_code && op_code[0] == '#')
+		return;
+	bus.args = strtok(NULL, "\n\t");
+	while (inst_op[a].opcode && op_code)
+	{
+		if (strcmp(op_code, inst_op[a].opcode) == 0)
+		{
+			inst_op[a].f(stack, line_no);
+			return (0);
+		}
+		a++;
+	}
+	if (op_code && inst_op[a].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_no, op_code);
+		fclose(file);
+		free(content);
+
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
+}
