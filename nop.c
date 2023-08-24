ad7317fe778554405316a3582dad58cfae409558
eugene kimani <ekiukkim90@gmail.com>
2023-08-24 07:08:52 -0700

    monty

--- nop.c
+++ nop.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * my_nop
- * @head:
- * @line_no:
+ * my_nop - doesn't do anything
+ * @head: head stack of the linked list
+ * @line_no: line number in bytecode
  * Return: No return
  */
 void my_nop(stack_t **head, unsigned int line_no)
